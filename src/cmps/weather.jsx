1f0b462291bfb710327f9b3803212b57ba517a1b
Gal Luski <luskigal@gmail.com>
2024-03-12 12:37:04 +0200

    weather cmp + weather icons

---
+++ src/cmps/weather.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { WEATHER_API_URL, WEATHER_API_KEY, lat, lon } from '../services/api'
+
+export function Weather() {
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const url = `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+            const response = await fetch(url)
+            const data = await response.json()
+
+            setWeatherData(data)
+            console.log(data)
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <div className="weather">
+            {weatherData && (
+                <>
+                    <img
+                        className="weather-icon"
+                        src={`./weather-icons/${weatherData.weather[0].icon}.svg`}
+                        alt={weatherData.weather[0].description}
+                    />
+                    <span className="temperature">{Math.round(weatherData.main.temp)}°C</span>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default Weather
